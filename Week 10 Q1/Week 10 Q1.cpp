84c7686c1512f08e268077b3302ffca80eba6fa0
NootNoot750f <nickholas195@gmail.com>
2025-03-15 22:51:00 -0500

    Add files via upload

---
+++ Week 10 Q1/Week 10 Q1.cpp
@@ -0,0 +1,10 @@
+// Week 10 Q1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
